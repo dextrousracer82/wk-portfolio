7801b42a9dcfd696d825e17fe47bdb4073791770
dextrousracer82 <95047057+dextrousracer82@users.noreply.github.com>
2024-06-30 13:28:25 -0700

    made catologue for chrome os exploits and added project ideas in projects folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html","./src/script.js"],
+  content: ["./src/index.html","./src/script.js","./src/exploitsOv.html","./src/projects/norg.html","./src/projects/asfc.html"],
   theme: {
     extend: {},
   },
